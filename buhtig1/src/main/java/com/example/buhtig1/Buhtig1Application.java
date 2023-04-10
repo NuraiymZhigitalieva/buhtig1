96bb1da51c4057b75b2b2315e2427c4207e85a41
NuraiymZhigitalieva <www.nurayim@gmail.com>
2023-04-10 15:00:16 +0600

    Initial commit

---
+++ buhtig1/src/main/java/com/example/buhtig1/Buhtig1Application.java
@@ -0,0 +1,13 @@
+package com.example.buhtig1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Buhtig1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Buhtig1Application.class, args);
+	}
+
+}
